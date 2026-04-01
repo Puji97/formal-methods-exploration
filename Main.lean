730f47334a96f067bd8bb1d822c9837f111f5cc0
Puji97 <110811680+Puji97@users.noreply.github.com>
2026-03-31 17:27:10 -0700

    Clean up: remove misplaced Lean project files from root

--- Main.lean
+++
@@ -1,4 +0,0 @@
-import TrafficLight
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- TrafficLight.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `TrafficLight` library.
--- Import modules here that should be built as part of the library.
-import TrafficLight.Basic
